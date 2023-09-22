caa7d9c6d12fbb6722992daf7c1bf0709f193d32
hugo.prigent <hugo.prigent@cpe.fr>
2023-09-22 11:17:50 +0200

    commit exo2

--- exectution.py
+++ exectution.py
@@ -5,4 +5,19 @@ Hugo Prigent
 """
 import test
 
-print(test.valid(30,4,2001))
+
+""" exercice 1: programme principal"""
+
+
+#jour=input("entrer un jou:")
+#mois=input("entrer un mois:")
+#annee=input("entrer une annee:")
+
+#if test.valid(int(jour),int(mois),int(annee)):
+    #print("la date est valide")
+#else:
+    #print("la date est non valide")
+    
+
+""" exercice 2"""
+print(test.mesImpots(50000))
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ hugo prigent
 
 """
 
-#exercice1:
+""" exercice1: """
 
 def biss(x):
     C1=False
@@ -37,5 +37,31 @@ def valid(jour,mois,annee): #format d'entre de la date
     if jour>ndays(mois):    #jour d'un mois impossible
         return False 
     
+    else:
+        return True
+    
+
+""" exercice 2"""
+
+def mesImpots(revenu):
+    
+    
+    #on rentre les paramètres pour les calculs:
+    T2=[10226,26070]
+    T3=[26071,74545]
+    T4=[74546,160336]
+    T5=160336
+    tx2=0.11
+    tx3=0.3
+    tx4=0.41
+    tx5=0.45
+    if revenu <=10225 :return (0)
+    if revenu <=T2[1] :return round ((revenu-T2[0])*tx2,1)
+    if revenu <=T3[1] :return round((revenu-T3[0])*tx3 +(T2[1]-T2[0])*tx2,1)
+    if revenu <=T4[1] :return round ((revenu -T4[0])*tx4 +(T3[1]-T3[0])*tx3 +(T2[1]-T2[0])*tx2,1 ) 
+    if revenu <=T5    :return round ((revenu-T5)*tx5 + (T4[1]-revenu)*tx4 +(T3[1]-T3[0])*tx3 +(T2[1]-T2[0])*tx2,1 )
+
+
+
 
 
